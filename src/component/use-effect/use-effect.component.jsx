e9068f521842033a03b648d324689e6e28648458
Marian Zburlea <marianzburlea@gmail.com>
2019-10-01 01:10:58 +0100

    use effect to load data

--- src/component/use-effect/use-effect.component.jsx
+++ src/component/use-effect/use-effect.component.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { list } from 'postcss'
 
 const UseEffectComponent = () => {
+  const [people, updatePeople] = useState([])
+  useEffect(() => {
+    fetch(`http://localhost:3456/people`)
+      .then(result => result.json())
+      .then(result => updatePeople(result))
+      .catch(error => console.log(error))
+  }, [])
+  console.log(people)
   return (
-    <div>Use Effect</div>
+    <div>
+      <h1>Use Effect</h1>
+      <h2>List of people</h2>
+      <ul>
+        {people.map(({ name, age }) => <li key={name}>{name} - {age}</li>)}
+      </ul>
+    </div>
   )
 }
 
